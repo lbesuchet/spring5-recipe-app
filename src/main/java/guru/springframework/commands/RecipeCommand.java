e9d3ee9e6a14ed89a224e9fdee8f465842184bdf
Luc Besuchet <luc.besuchet@kudelskisecurity.com>
2023-02-09 07:51:50 +0100

    controller advice

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -2,21 +2,36 @@ package guru.springframework.commands;
 import guru.springframework.domain.Difficulty;
 import java.util.HashSet;
 import java.util.Set;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class RecipeCommand {
   private Long id;
+  @NotBlank
+  @Size(min = 3, max = 255)
   private String description;
+  @Min(1)
+  @Max(999)
   private Integer prepTime;
+  @Min(1)
+  @Max(999)
   private Integer cookTime;
+  @Min(1)
+  @Max(100)
   private Integer servings;
   private String source;
+  @URL
   private String url;
+  @NotBlank
   private String directions;
   private Difficulty difficulty;
   private NotesCommand notes;
--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -112,4 +112,20 @@ public class RecipeControllerTest {
         .andExpect(status().isBadRequest())
         .andExpect(view().name("400Error"));
   }
+
+  @Test
+  public void testPostNewRecipeFormValidationFail() throws Exception {
+    RecipeCommand command = new RecipeCommand();
+    command.setId(2L);
+
+    when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+    mockMvc.perform(post("/recipe")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("id", "")
+        .param("cookTime", "3000"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("recipe/recipeform"))
+        .andExpect(model().attributeExists("recipe"));
+  }
 }
